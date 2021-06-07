5d2b62e50810aefbc5765a6a879679467e114e31
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-06-06 21:28:23 -0700

    added div and mult tests

--- test.py
+++ test.py
@@ -12,6 +12,16 @@ class TestCase(unittest.TestCase):
         self.assertEqual(calculator.sub(5, -5), 10)
         self.assertEqual(calculator.sub(10.5, 5.5), 5)
 
+    def test_multi(self):
+        self.assertEqual(calculator.mult(1, 2), 2)
+        self.assertEqual(calculator.mult(5, -5), -25)
+        self.assertEqual(calculator.mult(.5, .5), .25)
+
+    def test_div(self):
+        self.assertEqual(calculator.div(2, 1), 2)
+        self.assertEqual(calculator.div(25, 5), 5)
+        self.assertEqual(calculator.div(-100, 10), -10)
+
 
 # enables running directly
 if __name__ == "__main__":
