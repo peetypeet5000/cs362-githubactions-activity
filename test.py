123e9769d86246a8fa6c79345f9c015ba808cd05
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-06-06 21:25:38 -0700

    added sub tests

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ class TestCase(unittest.TestCase):
         self.assertEqual(calculator.add(1, 1), 2)
         self.assertEqual(calculator.add(-1, 1), 0)
 
+    def test_subtraction(self):
+        self.assertEqual(calculator.sub(1, 1), 0)
+        self.assertEqual(calculator.sub(5, -5), 10)
+        self.assertEqual(calculator.sub(10.5, 5.5), 5)
+
 
 # enables running directly
 if __name__ == "__main__":
