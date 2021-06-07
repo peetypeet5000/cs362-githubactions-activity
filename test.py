1c74ee77e0ac610a7ef60b0b7252dd52e58ead6f
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-06-06 21:22:06 -0700

    added addition method and tests

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import calculator
+
+class TestCase(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(calculator.add(1, 1), 2)
+        self.assertEqual(calculator.add(-1, 1), 0)
+
+#enables running directly
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
