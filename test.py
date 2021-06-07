108f49edbb26b87004d9f3a093a67db031c35336
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-06-06 21:24:13 -0700

    fix linting issues

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import unittest
 import calculator
 
+
 class TestCase(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(calculator.add(1, 1), 2)
         self.assertEqual(calculator.add(-1, 1), 0)
 
-#enables running directly
+
+# enables running directly
 if __name__ == "__main__":
     unittest.main(verbosity=2)
